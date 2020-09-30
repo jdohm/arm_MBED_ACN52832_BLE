508d3d5e5d404886db03ff168d803422748cc9da
JDohm <jannismax@gmail.com>
2020-10-01 01:38:11 +0200

    first steps to support GSR

--- source/main.cpp
+++ source/main.cpp
@@ -35,8 +35,10 @@
 #include "SparkFun_Bio_Sensor_Hub_Library.h"
 
 //Adding GSR to Bluetooth
-//#include "GSRService.h"
+#include "GSRService.h"
+AnalogIn adc_gsr1(p28);
 
+//HR
 I2C i2c(I2C_SDA0,I2C_SCL0);
   DigitalOut   resetPin(p26,0);
   DigitalInOut mfioPin(p25,PIN_OUTPUT,PullUp,0);
@@ -182,6 +184,134 @@ private:
     ble::AdvertisingDataBuilder _adv_data_builder;
 };
 
+class GSRLoggerC : ble::Gap::EventHandler {
+public:
+    GSRLoggerC(BLE &ble, events::EventQueue &event_queue) :
+        _ble(ble),
+        _event_queue(event_queue),
+        //_led1(LED1, 1),
+        _connected(false),
+        _gsr_uuid(GSRService::GSR_SERVICE_UUID),
+        _gsr_counter(100),
+        _gsr_service(ble, _gsr_counter, HeartRateService::LOCATION_FINGER),
+        _adv_data_builder(_adv_buffer) { }
+
+    void start() {
+        _ble.gap().setEventHandler(this);
+
+        _ble.init(this, &GSRLoggerC::on_init_complete);
+        //TODO Testdatenlogging mit Intervall von 100ms (wie Prototyp 1)
+        _event_queue.call_every(500ms, this, &GSRLoggerC::blink);
+        _event_queue.call_every(1s, this, &GSRLoggerC::update_sensor_value);
+
+        _event_queue.dispatch_forever();
+    }
+
+private:
+    /** Callback triggered when the ble initialization process has finished */
+    void on_init_complete(BLE::InitializationCompleteCallbackContext *params) {
+        if (params->error != BLE_ERROR_NONE) {
+            SEGGER_RTT_WriteString(0,"Ble initialization failed.\n");
+            return;
+        }
+
+        print_mac_address();
+
+        start_advertising();
+    }
+
+    void start_advertising() {
+        /* Create advertising parameters and payload */
+
+        ble::AdvertisingParameters adv_parameters(
+            ble::advertising_type_t::CONNECTABLE_UNDIRECTED,
+            ble::adv_interval_t(ble::millisecond_t(1000))
+        );
+        //TODO an GSR bzw Sensorsoeckhen Anpassen?!
+        _adv_data_builder.setFlags();
+        _adv_data_builder.setAppearance(ble::adv_data_appearance_t::UNKNOWN);
+        _adv_data_builder.setLocalServiceList(mbed::make_Span(&_gsr_uuid, 1));
+        _adv_data_builder.setName(DEVICE_NAME);
+
+        /* Setup advertising */
+
+        ble_error_t error = _ble.gap().setAdvertisingParameters(
+            ble::LEGACY_ADVERTISING_HANDLE,
+            adv_parameters
+        );
+
+        if (error) {
+            SEGGER_RTT_WriteString(0,"_ble.gap().setAdvertisingParameters() failed\r\n");
+            return;
+        }
+
+        error = _ble.gap().setAdvertisingPayload(
+            ble::LEGACY_ADVERTISING_HANDLE,
+            _adv_data_builder.getAdvertisingData()
+        );
+
+        if (error) {
+            SEGGER_RTT_WriteString(0,"_ble.gap().setAdvertisingPayload() failed\r\n");
+            return;
+        }
+
+        /* Start advertising */
+
+        error = _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+
+        if (error) {
+            SEGGER_RTT_WriteString(0,"_ble.gap().startAdvertising() failed\r\n");
+            return;
+        }
+    }
+
+    void update_sensor_value() {
+        if (_connected) {
+            _gsr_service.updateHeartRate(adc_gsr1.read_u16());
+        }
+    }
+
+    void blink(void) {
+        //_led1 = !_led1;
+        #ifdef CDEBUG
+        body = bioHub.readBpm();
+        SEGGER_RTT_printf(0,"Heartrate: %d\n",body.heartRate);
+        SEGGER_RTT_printf(0,"Confidence: %d\n",body.confidence);
+        SEGGER_RTT_printf(0,"Oxygen: %d\n",body.oxygen);
+        SEGGER_RTT_printf(0,"Status: %d\n",body.status);
+        #endif
+    }
+
+private:
+    /* Event handler */
+
+    void onDisconnectionComplete(const ble::DisconnectionCompleteEvent&) {
+        _ble.gap().startAdvertising(ble::LEGACY_ADVERTISING_HANDLE);
+        _connected = false;
+    }
+
+    virtual void onConnectionComplete(const ble::ConnectionCompleteEvent &event) {
+        if (event.getStatus() == BLE_ERROR_NONE) {
+            _connected = true;
+        }
+    }
+
+private:
+    BLE &_ble;
+    events::EventQueue &_event_queue;
+    //DigitalOut _led1;
+
+    bool _connected;
+
+    UUID _gsr_uuid;
+
+    uint8_t _gsr_counter;
+    HeartRateService _gsr_service;
+
+    uint8_t _adv_buffer[ble::LEGACY_ADVERTISING_MAX_SIZE];
+    ble::AdvertisingDataBuilder _adv_data_builder;
+};
+
 /** Schedule processing of events from the BLE middleware in the event queue. */
 void schedule_ble_events(BLE::OnEventsToProcessCallbackContext *context) {
     event_queue.call(Callback<void()>(&context->ble, &BLE::processEvents));
@@ -213,7 +343,7 @@ int main()
 
     ThisThread::sleep_for(1s);
 
-    HeartrateDemo demo(ble, event_queue);
+    GSRLoggerC demo(ble, event_queue);
     demo.start();
 
     return 0;
